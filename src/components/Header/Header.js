e5442e939dfc28858b91e0b66db238cfdb021928
AhamedJINabil <johayeriqbal@gmail.com>
2022-04-05 16:02:17 +0600

    initial commit and home section done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="font-bold mt-6 text-2xl">
+            <Link className="ml-4" to='/home'>Home</Link>
+            <Link className="ml-4" to='/reviews'>Reviews</Link>
+            <Link className="ml-4" to='/dashboard'>Dashboard</Link>
+            <Link className="ml-4" to='/blogs'>Blogs</Link>
+            <Link className="ml-4" to='/about'>About</Link>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import image from '../../images/yzr-m1.jpg'
+
+
+const Home = () => {
+
+
+    return (
+        <div className='lg:flex justify-center items-center mt-12'>
+            <div className='lg:w-2/4'>
+                <h1 className='text-5xl font-bold ml-8 '>Bike You Have Always <span className='text-indigo-900'>Dreamed About!</span></h1>
+                <p className='p-4 text-indigo-900'>The 2012 Yamaha YZR-M1 1,000cc inline four-cylinder
+                    machine will be ridden by factory riders Jorge Lorenzo and Ben Spies, as well as
+                    satellite riders Andrea Dovizioso and Cal Crutchlow on the Monster Yamaha Tech 3
+                    squad, as they attempt to overturn the Honda supremacy in 2012.</p>
+            </div>
+            <div className='lg:max-w-2xl pr-6 md:max-w-md p-6'>
+                <img className='rounded-lg' src={image} alt="" />
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Home;
